77cfb0f157bfb047007d9f0a56b4b5c19ec04870
Lucas Sta Maria <lucas@priime.dev>
2023-04-25 23:42:48 -0400

    chore: move `info.rkt` to project root
    This is to follow the proper structure of a Racket package.
    
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define deps '("base"))
 
 (define name "ralgo")
+(define collection 'multi)
 (define pkg-desc "Convenient implementations of various data structures for Racket")
 (define pkg-authors '(priime0))
 (define homepage "https://github.com/priime0/ralgo")
