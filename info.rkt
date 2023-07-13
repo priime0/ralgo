2326fda2852cf9734aeed7e1d8bb29b5a5f92b1d
Lucas Sta Maria <lucas@priime.dev>
2023-07-12 19:09:20 -0700

    chore: include all test paths
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- info.rkt
+++ info.rkt
@@ -1,11 +1,12 @@
 #lang info
 
-(define deps '("base"))
+(define deps '("base" "rackunit"))
 
 (define name "ralgo")
 (define collection 'multi)
 (define pkg-desc "Convenient implementations of various data structures for Racket")
 (define pkg-authors '(priime0))
 (define homepage "https://github.com/priime0/ralgo")
+(define test-include-paths 'all)
 (define version "0.1")
 (define license 'MIT)
