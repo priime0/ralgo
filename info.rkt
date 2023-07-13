5afbc3e7315bfe0f08442561e4ed47fec49d9fa4
Lucas Sta Maria <lucas@priime.dev>
2023-07-12 23:14:25 -0700

    chore: add `build-deps`, redefine collection, add documentation path
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- info.rkt
+++ info.rkt
@@ -1,12 +1,18 @@
 #lang info
 
 (define deps '("base" "rackunit"))
+(define build-deps '("scribble-lib"
+                     "scribble-text-lib"
+                     "racket-doc"
+                     "rackunit-doc"))
 
 (define name "ralgo")
-(define collection 'multi)
+(define collection "ralgo")
 (define pkg-desc "Convenient implementations of various data structures for Racket")
 (define pkg-authors '(priime0))
 (define homepage "https://github.com/priime0/ralgo")
-(define test-include-paths 'all)
 (define version "0.1")
 (define license 'MIT)
+
+(define test-include-paths 'all)
+(define scribblings '(("scribblings/manual.scrbl" ())))
